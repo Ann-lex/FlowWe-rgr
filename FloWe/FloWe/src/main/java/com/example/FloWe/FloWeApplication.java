93d7a1520c1963198e25586e66270cdee4f90612
Ann-lex <nastyachurina2006@gmail.com>
2026-05-18 21:27:06 +0400

    Исправление ошибки совместимости

---
+++ FloWe/FloWe/src/main/java/com/example/FloWe/FloWeApplication.java
@@ -0,0 +1,13 @@
+package com.example.FloWe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloWeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloWeApplication.class, args);
+	}
+
+}
---
+++ FloWe/FloWe/src/test/java/com/example/FloWe/FloWeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FloWe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloWeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
