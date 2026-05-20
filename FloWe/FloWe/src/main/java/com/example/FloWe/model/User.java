d5b295e4aef1af896410f66d99b1d219ab0533bb
gekkheild <gekkheild@gmail.com>
2026-05-20 15:20:31 +0400

    Добавлена модель пользователя и обновлен application

--- FloWe/FloWe/src/main/java/com/example/FloWe/model/User.java
+++ FloWe/FloWe/src/main/java/com/example/FloWe/model/User.java
@@ -1,4 +1,94 @@
 package com.example.FloWe.model;
 
+import java.math.BigDecimal;
+
 public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+    private boolean enabled;
+    private BigDecimal balance;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email,
+                String password, String role, boolean enabled, BigDecimal balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.enabled = enabled;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
